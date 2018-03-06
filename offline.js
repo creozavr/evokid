ada0c7a56a1889540a50b16cf766fe5f3d8033de
creozavr <creozavr@gmail.com>
2018-03-06 11:22:37 +0300

    Update 0.3

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1520323794,
+	"version": 1520324529,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -13,6 +13,7 @@
 		"icon-114.png",
 		"icon-128.png",
 		"icon-256.png",
-		"loading-logo.png"
+		"loading-logo.png",
+		"ga_init.js"
 	]
 }
